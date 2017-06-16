c8036fba709cd923ce8ce29a348850c3738b6c03
ruddyp <r.palvair@alptis.fr>
2017-06-16 13:54:43 +0200

    feat : Use applicationBuilder + private constructor

--- src/main/java/com/palvair/infrastructure/Application.java
+++ src/main/java/com/palvair/infrastructure/Application.java
@@ -1,12 +1,16 @@
 package com.palvair.infrastructure;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication(scanBasePackages = "com.palvair")
 public class Application {
 
+    private Application() {
+    }
+
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application.class, args);
+        new SpringApplicationBuilder(Application.class)
+                .run(args);
     }
 }
